efcd1c46f2d1e5651b0f7c4b853a3e1e566b9094
Saúl Laínez <saullainez@hotmail.es>
2021-06-02 11:18:01 -0600

    Personalizar el cuadro y disparar eventos del formulario

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar } from '../ui/Navbar';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'moment/locale/es';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { messages } from '../../helpers/calendar-messages-es';
+import { CalendarEvent } from './CalendarEvent';
 
 moment.locale('es');
 
@@ -15,10 +16,29 @@ const events = [{
     start: moment().toDate(),
     end: moment().add(2, 'hours').toDate(),
     bgcolor: '#fafafa',
-    notes: 'Comprar el pastel'
+    notes: 'Comprar el pastel',
+    user: {
+        _id: 'ABC',
+        name: 'Saul'
+    }
 }]
 
 export const CalendarScreen = () => {
+
+    const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'month')
+
+    const onDoubleClick = (e) => {
+        console.log(e);
+    }
+
+    const onSelectEvent = (e) => {
+        console.log(e);
+    }
+
+    const onViewChange = (e) => {
+        setLastView(e);
+        localStorage.setItem('lastView', e);
+    }
 
     const eventStyleGetter = (event, start, end, isSelected) => {
         const style = {
@@ -43,6 +63,13 @@ export const CalendarScreen = () => {
                 endAccessor="end"
                 messages={ messages }
                 eventPropGetter={ eventStyleGetter }
+                onDoubleClickEvent = {onDoubleClick}
+                onSelectEvent = {onSelectEvent}
+                onView={ onViewChange }
+                view = {lastView}
+                components={{
+                    event: CalendarEvent
+                }}
             />
         </div>
     )
