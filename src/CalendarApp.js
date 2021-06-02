7fc1b7995c97c2c5d6e2a37192b6352188e9bd31
Saúl Laínez <saullainez@hotmail.es>
2021-06-02 09:10:42 -0600

    Configurar sistema de rutas

--- src/CalendarApp.js
+++ src/CalendarApp.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { AppRouter } from './router/AppRouter'
 
 export const CalendarApp = () => {
     return (
         <div>
-            <h1>Hola mundo</h1>
+            <AppRouter />
         </div>
     )
 }
