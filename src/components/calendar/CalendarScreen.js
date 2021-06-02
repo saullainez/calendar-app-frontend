3b9e21627dd85ce88071c55a5783a3d6586d05e6
Saúl Laínez <saullainez@hotmail.es>
2021-06-02 10:52:43 -0600

    Configuraciones adicionales del calendario

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { Navbar } from '../ui/Navbar';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
+import 'moment/locale/es';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { messages } from '../../helpers/calendar-messages-es';
+
+moment.locale('es');
 
 const localizer = momentLocalizer(moment);
 
@@ -10,10 +14,24 @@ const events = [{
     title: 'Cumpleaños',
     start: moment().toDate(),
     end: moment().add(2, 'hours').toDate(),
-    bgcolor: '#fafafa'
+    bgcolor: '#fafafa',
+    notes: 'Comprar el pastel'
 }]
 
 export const CalendarScreen = () => {
+
+    const eventStyleGetter = (event, start, end, isSelected) => {
+        const style = {
+            backgrounColor: '#367CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+        }
+        return {
+            style
+        }
+    }
+
     return (
         <div className="calendar-screen">
             <Navbar />
@@ -23,6 +41,8 @@ export const CalendarScreen = () => {
                 events={ events }
                 startAccessor="start"
                 endAccessor="end"
+                messages={ messages }
+                eventPropGetter={ eventStyleGetter }
             />
         </div>
     )
