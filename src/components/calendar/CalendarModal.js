beac9ab39cda349d1ad59d14b0f65cb97ec28c2d
Saúl Laínez <saullainez@hotmail.es>
2021-06-04 09:18:28 -0600

    Obtener la información del formulario del evento

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -12,95 +12,134 @@ const customStyles = {
       marginRight           : '-50%',
       transform             : 'translate(-50%, -50%)'
     }
-  };
-  Modal.setAppElement('#root');
+};
 
-  const now = moment().minutes(0).seconds(0).add(1, 'hours');
-  const nowPlusOne = now.clone().add(1, 'hours');
+Modal.setAppElement('#root');
+
+const now = moment().minutes(0).seconds(0).add(1, 'hours');
+const nowPlusOne = now.clone().add(1, 'hours');
 
 export const CalendarModal = () => {
 
-  const [dateStart, setDateStart] = useState( now.toDate() );
-  const [dateEnd, setDateEnd] = useState( nowPlusOne.toDate() );
+  const [ dateStart, setDateStart ] = useState( now.toDate() );
+  const [ dateEnd, setDateEnd ] = useState( nowPlusOne.toDate() );
 
-    const closeModal = () => {
+  const [ formValues, setFormValues ] = useState({
+    title: 'Evento',
+    notes: '',
+    start: now.toDate(),
+    end: nowPlusOne.toDate()
+  });
 
-    }
+  const { notes, title } = formValues;
 
-    const handleStartDateChange = ( e ) => {
-      setDateStart( e );
-    }
+  const handleInoutChange = ({target}) => {
+    setFormValues({
+      ...formValues,
+      [target.name]: target.value
+    })
+  }
 
-    const handleEndDateChange = ( e ) => {
-      setDateEnd( e );
-    }
+  const closeModal = () => {
 
-    return (
-        <Modal
-          isOpen={ true }
-          //onAfterOpen={afterOpenModal}
-          onRequestClose={closeModal}
-          style={ customStyles }
-          closeTimeoutMS={200}
-          className="modal"
-          overlayClassName="modal-fondo"
-        >
-            <h1> Nuevo evento </h1>
-            <hr />
-            <form className="container">
+  }
 
-                <div className="form-group">
-                    <label>Fecha y hora inicio</label>
-                          <DateTimePicker
-                            onChange={ handleStartDateChange }
-                            value={ dateStart }
-                            className="form-control"
-                          />
-                </div>
+  const handleStartDateChange = ( e ) => {
+    setDateStart( e );
+    setFormValues({
+      ...formValues,
+      start: e
+    });
+  }
 
-                <div className="form-group">
-                    <label>Fecha y hora fin</label>
-                    <DateTimePicker
-                      onChange={ handleEndDateChange }
-                      value={ dateEnd }
+  const handleEndDateChange = ( e ) => {
+    setDateEnd( e );
+    setFormValues({
+      ...formValues,
+      end: e
+    });
+  }
+
+  const handleSubmitForm = ( e ) => {
+    e.preventDefault();
+
+    console.log(formValues);
+
+  }
+
+  return (
+      <Modal
+        isOpen={ true }
+        //onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={ customStyles }
+        closeTimeoutMS={200}
+        className="modal"
+        overlayClassName="modal-fondo"
+      >
+          <h1> Nuevo evento </h1>
+          <hr />
+          <form 
+            className="container"
+            onSubmit={ handleSubmitForm }
+          >
+
+              <div className="form-group">
+                  <label>Fecha y hora inicio</label>
+                        <DateTimePicker
+                          onChange={ handleStartDateChange }
+                          value={ dateStart }
+                          className="form-control"
+                        />
+              </div>
+
+              <div className="form-group">
+                  <label>Fecha y hora fin</label>
+                  <DateTimePicker
+                    onChange={ handleEndDateChange }
+                    value={ dateEnd }
+                    className="form-control"
+                    minDate={ dateStart }
+                  />
+              </div>
+
+              <hr />
+              <div className="form-group">
+                  <label>Titulo y notas</label>
+                  <input 
+                      type="text" 
                       className="form-control"
-                      minDate={ dateStart }
-                    />
-                </div>
+                      placeholder="Título del evento"
+                      name="title"
+                      autoComplete="off"
+                      value={ title }
+                      onChange={ handleInoutChange }
+                  />
+                  <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
+              </div>
 
-                <hr />
-                <div className="form-group">
-                    <label>Titulo y notas</label>
-                    <input 
-                        type="text" 
-                        className="form-control"
-                        placeholder="Título del evento"
-                        name="title"
-                        autoComplete="off"
-                    />
-                    <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
-                </div>
+              <div className="form-group">
+                  <textarea 
+                      type="text" 
+                      className="form-control"
+                      placeholder="Notas"
+                      rows="5"
+                      name="notes"
+                      value={ notes }
+                      onChange={ handleInoutChange }
+                  ></textarea>
+                  <small id="emailHelp" className="form-text text-muted">Información adicional</small>
+              </div>
 
-                <div className="form-group">
-                    <textarea 
-                        type="text" 
-                        className="form-control"
-                        placeholder="Notas"
-                        rows="5"
-                        name="notes"
-                    ></textarea>
-                    <small id="emailHelp" className="form-text text-muted">Información adicional</small>
-                </div>
+              <button
+                  type="submit"
+                  className="btn btn-outline-primary btn-block"
+              >
+                  <i className="far fa-save"></i>
+                  <span> Guardar</span>
+              </button>
 
-                <button
-                    type="submit"
-                    className="btn btn-outline-primary btn-block"
-                >
-                    <i className="far fa-save"></i>
-                    <span> Guardar</span>
-                </button>
-
-            </form>
-        </Modal>
-    )
+          </form>
+      </Modal>
+  )
 }
